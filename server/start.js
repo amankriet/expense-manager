11873a4139f7c45a0193d7b2dae858d7f0215147
Aman Kumar <amankriet@gmail.com>
2026-05-04 20:16:22 +0530

    fix: added default host for ecr resolution

--- server/start.js
+++ server/start.js
@@ -3,9 +3,10 @@ import app from "./index.js";
 import { connectDB } from "./config/database.js";
 
 const port = process.env.PORT || 3001;
+const host = "0.0.0.0";
 
 await connectDB();
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, host, () => {
+    console.log(`Server running on ${host}:${port}`);
 });
